edfb226468b04d644f7e1b81fe1bae55be56c1d4
BriceMSG <brice.b@my-serious-game.com>
2017-04-10 19:06:45 +0200

    db

--- db-init.sh
+++ db-init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo Moving mysql db
-#if [ ! -d /data/mysql ]; then
-	#mkdir -p /data/mysql
-	#cp -r /var/lib/mysql/* /data/mysql
-	#rm -rf /var/lib/mysql
-	#chown -R mysql:mysql /data/mysql
-	mysql -h 127.0.0.1 -P 3306 -u root < /home/alteretgo/alter.sql
-#fi
+if [ ! -d /data/mysql ]; then
+	mkdir -p /data/mysql
+	cp -r /var/lib/mysql/* /data/mysql
+	rm -rf /var/lib/mysql
+	chown -R mysql:mysql /data/mysql
+  sed -i -e "s@^datadir.*@datadir = /data/mysql@" /etc/mysql/my.cnf
+  /etc/init.d/mysql restart
+	mysql -u root < /home/alteretgo/alter.sql
+fi
