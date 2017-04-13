d05f64dbd57b225ebd94756279fc15b4065f31b9
BriceMSG <brice.b@my-serious-game.com>
2017-04-13 12:31:53 +0200

    added js Scripts in php include

---
+++ app/interfaces/formateur/include/jsScripts.inc.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/jquery.min.js"></script>
+<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/socket.io-1.2.0.js"></script>
+<!--<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/js/gaia_lcs.js"></script>-->
+<!--<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/js/gaia_lcs_func.js"></script>-->
+<script src="http://commun.alteretgo.my-workflow.fr/formateur.js"></script>
--- app/interfaces/formateur/session/photolangage.php
+++ app/interfaces/formateur/session/photolangage.php
@@ -133,15 +133,9 @@
 	<?php
 		include '../include/notification.inc.php';
 		include '../include/screenRotation.inc.php';
+		include '../include/jsScripts.inc.php';
 	?>
 
-	<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/jquery.min.js"></script>
-	<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/socket.io-1.2.0.js"></script>
-
-	<!--<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/js/gaia_lcs.js"></script>-->
-	<!--<script type="text/javascript" src="http://commun.alteretgo.my-workflow.fr/js/gaia_lcs_func.js"></script>-->
-
-	<script src="http://commun.alteretgo.my-workflow.fr/formateur.js"></script>
 	<script type="text/javascript">
 		var it_io = 0, io_try = 5;
 		function checkIo() {
